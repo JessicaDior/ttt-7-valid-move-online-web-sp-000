765c0946db86aeb64da2b90b5cd38422dbd83c37
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-12 01:58:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,10 +4,13 @@ def valid_move?(board, index)
   if index.between?(0,  8) && !position_taken(board, index)
     return true
   end
+end
+
 def position_taken?(board, index)
 if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
   return false
 else
   return true
 end
+
 end
